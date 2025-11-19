435cbecd353094e3a7e80669d7a1b7d4ba67f369
Lope MS <lopems@gmail.com>
2025-11-19 20:35:32 +0100

    Create Footer.jsx

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <div className="mt-auto">
+        <hr/>
+        <p>
+          Made by{": "}
+          <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with	love!
+        </p>
+    </div>
+  )
+}
